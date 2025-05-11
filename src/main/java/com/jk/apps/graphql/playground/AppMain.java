a6eefb79e0e0cd41d61b2fbe4b9f2a0d9e9327e4
Kishore <>
2025-05-10 20:35:27 -0400

    Added AppMain

---
+++ src/main/java/com/jk/apps/graphql/playground/AppMain.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.jk.apps.graphql.playground;
+
+/**
+ * 
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(AppMain.class, args);
+	}
+
+}
