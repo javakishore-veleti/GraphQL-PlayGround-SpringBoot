0cefa2e7d9d9ccfe182159c248d93125b3511c16
Kishore <>
2025-05-10 20:26:49 -0400

    Initial commit of Maven project

---
+++ src/main/java/com/jk/apps/graphql/playground/App.java
@@ -0,0 +1,10 @@
+package com.jk.apps.graphql.playground;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
